8afccee09d3909ba946c3d9eadd47498f7dc7103
Ravi Shankar <ravi.shankar@manch.app>
2018-12-28 17:14:05 +0530

    fix repost-subscriber difference count

--- pkg/callbacks/repost-subscriber.go
+++ pkg/callbacks/repost-subscriber.go
@@ -25,9 +25,12 @@ func RepostSubscriberCB(subj, reply string, p *subscribers.Post) {
 
 	if post.Created.UserType == "general" && post.UpVotes < 30 {
 		// schedule votes to reach 30 votes
+		fmt.Println("General user....")
 		diff := 30 - post.UpVotes
+		fmt.Println("total difference is", diff)
 		j := 0
 		// schedule 5 votes in 30 minutes
+		total_votes := diff
 		if diff > 0 {
 			diff -= 5
 			t := utils.SplitTimeInRange(1*60, 30*60, 5, time.Second)
@@ -39,17 +42,24 @@ func RepostSubscriberCB(subj, reply string, p *subscribers.Post) {
 		// schedule rest votes in next 1.30 hours
 		if diff > 0 {
 			t := utils.SplitTimeInRange(30, 120, diff, time.Minute)
-			for k := 0; j < diff; j, k = j+1, k+1 {
+			for k := 0; j < total_votes; j, k = j+1, k+1 {
 				vote := mongo.CreateVotesSchedulePost(t[k], bson.ObjectIdHex(p.Id), bson.ObjectIdHex(botProfilesIds[j]))
 				mongo.AddVoteSchedule(vote)
 			}
 		}
+		fmt.Println("total votes added: ", j)
 
 	} else if post.Created.UserType == "bot" && post.UpVotes < 45 {
+		fmt.Println("Bot user....")
+
 		// schedule votes to reach 45 votes
 		diff := 45 - post.UpVotes
+		fmt.Println("total difference is", diff)
+		
 		j := 0
 		// schedule 5 votes in 30 minutes
+		total_votes := diff
+
 		if diff > 0 {
 			diff -= 5
 			t := utils.SplitTimeInRange(1*60, 30*60, 5, time.Second)
@@ -61,10 +71,14 @@ func RepostSubscriberCB(subj, reply string, p *subscribers.Post) {
 		// schedule rest votes in next 1.30 hours
 		if diff > 0 {
 			t := utils.SplitTimeInRange(30, 120, diff, time.Minute)
-			for k := 0; j < diff; j, k = j+1, k+1 {
+			for k := 0; j < total_votes; j, k = j+1, k+1 {
 				vote := mongo.CreateVotesSchedulePost(t[k], bson.ObjectIdHex(p.Id), bson.ObjectIdHex(botProfilesIds[j]))
 				mongo.AddVoteSchedule(vote)
 			}
 		}
+		fmt.Println("total votes added: ", j)
+
 	}
+	fmt.Printf("Processed a post on subject %s! with Post Id %s\n", subj, p.Id)
+
 }
