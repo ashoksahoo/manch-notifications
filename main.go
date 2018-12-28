f82f8ba9a0a9d8dec0a97425a069bc14769764c5
Ravi Shankar <ravi.shankar@manch.app>
2018-12-28 13:34:21 +0530

    add comment on main file

--- main.go
+++ main.go
@@ -15,22 +15,31 @@ func main() {
 		w.Write([]byte("pong"))
 	})
 
+	// received a post
 	subscribers.PostSubscriber(callbacks.PostSubscriberCB)
 
+	// received a comment
 	subscribers.CommentSubscriber(callbacks.CommentSubscriberCB)
 
+	// received a vote on post
 	subscribers.VotePostSubscriber(callbacks.VotePostSubscriberCB)
 
+	// received a vote on comment
 	subscribers.VoteCommentSubscriber(callbacks.VoteCommentSubscriberCB)
 
+	// received a new user
 	subscribers.UserSubscriber(callbacks.UserSubscriberCB)
 
+	// received user-follow
 	subscribers.UserFollowSubscriber(callbacks.UserFollowSubscriberCB)
 
+	// received post removed (soft delete)
 	subscribers.PostRemovedSubscriber(callbacks.PostRemovedSubscriberCB)
 
+	// received user-follow removed
 	subscribers.UserFollowRemovedSubscriber(callbacks.UserFollowRemovedSubscriberCB)
 
+	// listen on http server 5000
 	http.ListenAndServe(":5000", r)
 }
 
