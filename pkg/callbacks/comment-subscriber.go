275b35c4e44db733a0b7e91ccdfbfcaad3c6da4c
Ravi Shankar <ravi.shankar@manch.app>
2018-12-28 12:30:54 +0530

    refactor main file

---
+++ pkg/callbacks/comment-subscriber.go
@@ -0,0 +1,140 @@
+package callbacks
+
+import (
+	"fmt"
+	"notification-service/pkg/firebase"
+	"notification-service/pkg/i18n"
+	"notification-service/pkg/mongo"
+	"notification-service/pkg/subscribers"
+	"notification-service/pkg/utils"
+	"strconv"
+	"strings"
+
+	"github.com/globalsign/mgo/bson"
+)
+
+/**
+	This processes Comments from Posts
+	1) Get Comment Details and Unique commentator count
+	2) Validate self comment
+	3) Get Who created the post -> He gets the notification and we need his current lang
+	4) Get tokens from the above profile (Supports multiple device tokens.)
+	5) Create/Update Notification Table which has the meta info for the notificaiotn
+	6) Construct Data for i18n template
+	7) Generate template using template data and String Formatter
+	8) Create push notification
+	9) Fire the notifications in routines.
+
+	*/
+func CommentSubscriberCB(subj, reply string, c *subscribers.Comment) {
+	//fmt.Printf("\nNats MSG %+v", c)
+	fmt.Printf("Received a comment on subject %s! with Comment %+v\n", subj, c)
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in subscribers.CommentSubscriber", r)
+		}
+	}()
+	comment, uniqueCommentator := mongo.GetFullCommentById(c.Id)
+
+	// replied on Comment
+	// replyOnCommentId := comment.CommentId
+	// replyOnComment := mongo.GetCommentById(replyOnCommentId.Hex())
+	// var replyOnComment bson.ObjectId
+	var replyOnComment mongo.CommentModel
+	if len(comment.Parents) >= 2 {
+		replyOnComment = mongo.GetCommentById(comment.CommentId.Hex())
+	}
+	// comment on comment
+	if len(comment.Parents) >= 2 && replyOnComment.Created.ProfileId != comment.Created.ProfileId {
+		fmt.Println("reply on comments")
+		// if replyOnComment.Created.ProfileId == comment.Created.ProfileId {
+		// 	// Self Reply on comment
+		// 	fmt.Println("Self reply on comments")
+		// 	return;
+		// }
+		fmt.Printf("reply on comments %+v\n", replyOnComment)
+		// get replied on comment creator
+		replyOnCommentCreator := mongo.GetProfileById(replyOnComment.Created.ProfileId)
+		notification1 := mongo.CreateNotification(replyOnComment.Id, "comment", "comment", comment.Created.ProfileId)
+		tokens1 := mongo.GetTokensByProfiles([]bson.ObjectId{replyOnComment.Created.ProfileId})
+		// comment title
+		count := len(notification1.UniqueUsers) - 1
+		fmt.Println("Comment count: ", count)
+		commentTitle := utils.TruncateTitle(replyOnComment.Content, 4)
+		data1 := i18n.DataModel{
+			Name:    comment.Created.Name,
+			Comment: commentTitle,
+			Count:   count,
+		}
+		var msgStr1 string
+		if count > 0 {
+			msgStr1 = i18n.GetString(replyOnCommentCreator.Language, "comment_reply_multi", data1)
+		} else {
+			msgStr1 = i18n.GetString(replyOnCommentCreator.Language, "comment_reply_one", data1)
+		}
+		fmt.Println("message string: ", msgStr1)
+		title := i18n.GetAppTitle(replyOnCommentCreator.Language)
+		msgStr1 = strings.Replace(msgStr1, "\"\" ", "", 1)
+		msg := firebase.ManchMessage{
+			Title:      title,
+			Message:    msgStr1,
+			Icon:       mongo.ExtractThumbNailFromPost(comment.Post),
+			DeepLink:   "manch://posts/" + comment.PostId.Hex(),
+			BadgeCount: strconv.Itoa(replyOnComment.CommentCount),
+			Id:         notification1.Identifier,
+		}
+		fmt.Printf("\nGCM Message %+v\n", msg)
+		if tokens1 != nil {
+			for _, token := range tokens1 {
+				go firebase.SendMessage(msg, token.Token)
+			}
+		} else {
+			fmt.Printf("No token")
+		}
+		fmt.Println("end reply on comments")
+	}
+
+	if comment.Post.Created.ProfileId == comment.Created.ProfileId {
+		//Self comment
+		fmt.Println("Self Comment")
+		return
+	}
+	postCreator := mongo.GetProfileById(comment.Post.Created.ProfileId)
+	tokens := mongo.GetTokensByProfiles([]bson.ObjectId{comment.Post.Created.ProfileId})
+	notification := mongo.CreateNotification(comment.PostId, "comment", "post", comment.Post.Created.ProfileId)
+
+	postTitle := utils.TruncateTitle(comment.Post.Title, 4)
+	data := i18n.DataModel{
+		Name:  comment.Created.Name,
+		Count: uniqueCommentator - 1,
+		Post:  postTitle,
+	}
+	var msgStr string
+
+	if uniqueCommentator > 1 {
+		msgStr = i18n.GetString(postCreator.Language, "comment_multi", data)
+	} else {
+		msgStr = i18n.GetString(postCreator.Language, "comment_one", data)
+	}
+	title := i18n.GetAppTitle(postCreator.Language)
+	msgStr = strings.Replace(msgStr, "\"\" ", "", 1)
+	msg := firebase.ManchMessage{
+		Title:      title,
+		Message:    msgStr,
+		Icon:       mongo.ExtractThumbNailFromPost(comment.Post),
+		DeepLink:   "manch://posts/" + comment.PostId.Hex(),
+		BadgeCount: strconv.Itoa(comment.Post.CommentCount),
+		Id:         notification.Identifier,
+	}
+	fmt.Printf("\nGCM Message %+v\n", msg)
+	//firebase.SendMessage(msg, "frgp37gfvFg:APA91bHbnbfoX-bp3M_3k-ceD7E4fZ73fcmVL4b5DGB5cQn-fFEvfbj3aAI9g0wXozyApIb-6wGsJauf67auK1p3Ins5Ff7IXCN161fb5JJ5pfBnTZ4LEcRUatO6wimsbiS7EANoGDr4")
+	if tokens != nil {
+		for _, token := range tokens {
+			go firebase.SendMessage(msg, token.Token)
+		}
+	} else {
+		fmt.Printf("No token")
+	}
+	fmt.Printf("Processed a comment on subject %s! with Comment %s\n", subj, c.Id)
+
+}
---
+++ pkg/callbacks/post-removed-subscriber.go
@@ -0,0 +1,57 @@
+package callbacks
+
+import (
+	"fmt"
+	"notification-service/pkg/firebase"
+	"notification-service/pkg/i18n"
+	"notification-service/pkg/mongo"
+	"notification-service/pkg/subscribers"
+	"notification-service/pkg/utils"
+	"strings"
+
+	"github.com/globalsign/mgo/bson"
+)
+
+func PostRemovedSubscriberCB(subj, reply string, p *subscribers.Post) {
+	fmt.Printf("Received a post on subject %s! with Post %+v\n", subj, p)
+	post := mongo.GetPostById(p.Id)
+
+	postCreator := mongo.GetProfileById(post.Created.ProfileId)
+
+	tokens := mongo.GetTokensByProfiles([]bson.ObjectId{post.Created.ProfileId})
+	notification := mongo.CreateNotification(post.Id, "delete", "post", postCreator.Id)
+
+	reason := post.IgnoreReason
+	language := postCreator.Language
+	deleteReason := i18n.DeleteReason[language][reason]
+
+	postTitle := utils.TruncateTitle(post.Title, 4)
+	data := i18n.DataModel{
+		Name:         postCreator.Name,
+		Post:         postTitle,
+		DeleteReason: deleteReason,
+	}
+	var msgStr string
+	msgStr = i18n.GetString(language, "post_removed", data)
+	fmt.Println(msgStr)
+	title := i18n.GetAppTitle(language)
+	msgStr = strings.Replace(msgStr, "\"\" ", "", 1)
+	msg := firebase.ManchMessage{
+		Title:    title,
+		Message:  msgStr,
+		Icon:     mongo.ExtractThumbNailFromPost(post),
+		DeepLink: "manch://profile/" + postCreator.Id.Hex(),
+		Id:       notification.Identifier,
+	}
+
+	fmt.Printf("\nGCM Message %+v\n", msg)
+	if tokens != nil {
+		for _, token := range tokens {
+			go firebase.SendMessage(msg, token.Token)
+		}
+	} else {
+		fmt.Printf("No token")
+	}
+	fmt.Printf("Processed a post on subject %s! with Post ID %s\n", subj, p.Id)
+
+}
