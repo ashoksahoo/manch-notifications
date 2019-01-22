1f457aeb10c4538dc0ca700519105ef09a8895d4
Ravi Shankar <ravi.shankar@manch.app>
2019-01-22 13:18:53 +0530

    add bucketing for follow notificaiton

--- pkg/callbacks/user-follow-subscriber.go
+++ pkg/callbacks/user-follow-subscriber.go
@@ -128,19 +128,19 @@ func UserFollowSubscriberCB(subj, reply string, uf *subscribers.Subscription) {
 		DeepLink: "manch://profile/" + followsTo.Id.Hex(),
 		Id:       notification.NId,
 	}
-	//firebase.SendMessage(msg, "frgp37gfvFg:APA91bHbnbfoX-bp3M_3k-ceD7E4fZ73fcmVL4b5DGB5cQn-fFEvfbj3aAI9g0wXozyApIb-6wGsJauf67auK1p3Ins5Ff7IXCN161fb5JJ5pfBnTZ4LEcRUatO6wimsbiS7EANoGDr4")
-	fmt.Printf("\nGCM Message %+v\n", msg)
-	if tokens != nil {
-		for _, token := range tokens {
-			go firebase.SendMessage(msg, token.Token, notification)
+
+	folllowNumbers := []int{1, 2, 3, 5, 25, 50, 75, 100, 200, 500, 1000}
+
+	if utils.Contains(folllowNumbers, count + 1) {
+		//firebase.SendMessage(msg, "frgp37gfvFg:APA91bHbnbfoX-bp3M_3k-ceD7E4fZ73fcmVL4b5DGB5cQn-fFEvfbj3aAI9g0wXozyApIb-6wGsJauf67auK1p3Ins5Ff7IXCN161fb5JJ5pfBnTZ4LEcRUatO6wimsbiS7EANoGDr4")
+		fmt.Printf("\nGCM Message %+v\n", msg)
+		if tokens != nil {
+			for _, token := range tokens {
+				go firebase.SendMessage(msg, token.Token, notification)
+			}
+		} else {
+			fmt.Printf("No token\n")
 		}
-	} else {
-		fmt.Printf("No token\n")
 	}
-
 	fmt.Printf("Processed a User follow on subject %s! with user follow ID %s\n", subj, uf.Id)
-
 }
-
-
-
