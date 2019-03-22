665d2659146e18a78c94c10982e33b2f88bda67f
Ravi Shankar <ravi.shankar@manch.app>
2019-03-22 19:42:06 +0530

    fix followers count condition

--- pkg/callbacks/community-followers-update-subscriber.go
+++ pkg/callbacks/community-followers-update-subscriber.go
@@ -19,100 +19,97 @@ func CommunityFollowersUpdateCB(subj, reply string, C *subscribers.Community) {
 		}
 	}()
 	fmt.Println("followers count is ", C.FollowersCount)
-	if C.FollowersCount != 10 || C.FollowersCount != 100 {
-		fmt.Println("returning followers count is not 10 and 100")
-		return
-	}
 
-	community := mongo.GetCommunityById(C.Id)
+	if C.FollowersCount == 10 || C.FollowersCount == 100 {
+		community := mongo.GetCommunityById(C.Id)
+		if community.Type == "m_manch" {
 
-	if community.Type == "m_manch" {
+			fmt.Println("community type is", community.Type)
+			admins := community.Admins
+			adminProfilesIds := []string{}
 
-		fmt.Println("community type is", community.Type)
-		admins := community.Admins
-		adminProfilesIds := []string{}
+			for _, admin := range admins {
+				adminProfilesIds = append(adminProfilesIds, admin.ProfileId.Hex())
+			}
 
-		for _, admin := range admins {
-			adminProfilesIds = append(adminProfilesIds, admin.ProfileId.Hex())
-		}
+			adminProfiles := mongo.GetProfilesByIds(adminProfilesIds)
 
-		adminProfiles := mongo.GetProfilesByIds(adminProfilesIds)
+			entities := []mongo.Entity{
+				{
+					EntityId:   community.Id,
+					EntityType: "community",
+				},
+			}
 
-		entities := []mongo.Entity{
-			{
-				EntityId:   community.Id,
-				EntityType: "community",
-			},
-		}
+			data := i18n.DataModel{
+				Community: community.Name,
+			}
 
-		data := i18n.DataModel{
-			Community: community.Name,
-		}
-
-		var purpose, templateName, templateText string
-		if C.FollowersCount == 10 {
-			fmt.Println("updating template name for 10the followers")
-			templateName = "manch_10_members_title"
-			templateText = "manch_10_members_text"
-			purpose = constants.NotificationPurpose["MANCH_10_MEMBERS"]
-		} else if C.FollowersCount == 100 {
-			templateName = "manch_100_members"
-			purpose = constants.NotificationPurpose["MANCH_100_MEMBERS"]
-		}
-		deepLink := ""
-		for _, adminProfile := range adminProfiles {
-			var htmlMsgStr, msgStr, title string
+			var purpose, templateName, templateText string
 			if C.FollowersCount == 10 {
-				msgStr = i18n.GetString(adminProfile.Language, templateText, data)
-				htmlMsgStr = i18n.GetHtmlString(adminProfile.Language, templateName, data)
-				title = i18n.GetString(adminProfile.Language, templateName, data)
-			} else {
-				msgStr = i18n.GetString(adminProfile.Language, templateName, data)
-				htmlMsgStr = i18n.GetHtmlString(adminProfile.Language, templateName, data)
-				title = i18n.GetAppTitle(adminProfile.Language)
+				fmt.Println("updating template name for 10the followers")
+				templateName = "manch_10_members_title"
+				templateText = "manch_10_members_text"
+				purpose = constants.NotificationPurpose["MANCH_10_MEMBERS"]
+			} else if C.FollowersCount == 100 {
+				templateName = "manch_100_members"
+				purpose = constants.NotificationPurpose["MANCH_100_MEMBERS"]
 			}
-			fmt.Println("message str is ", msgStr)
-			fmt.Println("title is", title)
-			messageMeta := mongo.MessageMeta{
-				TemplateName: templateName,
-				Template:     i18n.Strings[adminProfile.Language][templateName],
-				Data:         data,
-			}
-
-			notification := mongo.CreateNotification(mongo.NotificationModel{
-				Receiver:        adminProfile.Id,
-				Identifier:      adminProfile.Id.Hex() + purpose,
-				Participants:    []bson.ObjectId{adminProfile.Id},
-				DisplayTemplate: constants.NotificationTemplate["TRANSACTIONAL"],
-				EntityGroupId:   community.Id.Hex(),
-				ActionId:        community.Id,
-				ActionType:      "community",
-				Purpose:         purpose,
-				Entities:        entities,
-				Message:         msgStr,
-				MessageHtml:     htmlMsgStr,
-				DeepLink:        deepLink,
-				MessageMeta:     messageMeta,
-			})
-
-			tokens := mongo.GetTokensByProfiles([]bson.ObjectId{adminProfile.Id})
-
-			msg := firebase.ManchMessage{
-				Title:    title,
-				Message:  msgStr,
-				DeepLink: deepLink,
-				Id:       notification.NId,
-			}
-
-			fmt.Printf("\nGCM Message %+v\n", msg)
-			if tokens != nil {
-				for _, token := range tokens {
-					go firebase.SendMessage(msg, token.Token, notification)
+			deepLink := ""
+			for _, adminProfile := range adminProfiles {
+				var htmlMsgStr, msgStr, title string
+				if C.FollowersCount == 10 {
+					msgStr = i18n.GetString(adminProfile.Language, templateText, data)
+					htmlMsgStr = i18n.GetHtmlString(adminProfile.Language, templateName, data)
+					title = i18n.GetString(adminProfile.Language, templateName, data)
+				} else {
+					msgStr = i18n.GetString(adminProfile.Language, templateName, data)
+					htmlMsgStr = i18n.GetHtmlString(adminProfile.Language, templateName, data)
+					title = i18n.GetAppTitle(adminProfile.Language)
 				}
-			} else {
-				fmt.Printf("No token\n")
-			}
+				fmt.Println("message str is ", msgStr)
+				fmt.Println("title is", title)
+				messageMeta := mongo.MessageMeta{
+					TemplateName: templateName,
+					Template:     i18n.Strings[adminProfile.Language][templateName],
+					Data:         data,
+				}
 
+				notification := mongo.CreateNotification(mongo.NotificationModel{
+					Receiver:        adminProfile.Id,
+					Identifier:      adminProfile.Id.Hex() + purpose,
+					Participants:    []bson.ObjectId{adminProfile.Id},
+					DisplayTemplate: constants.NotificationTemplate["TRANSACTIONAL"],
+					EntityGroupId:   community.Id.Hex(),
+					ActionId:        community.Id,
+					ActionType:      "community",
+					Purpose:         purpose,
+					Entities:        entities,
+					Message:         msgStr,
+					MessageHtml:     htmlMsgStr,
+					DeepLink:        deepLink,
+					MessageMeta:     messageMeta,
+				})
+
+				tokens := mongo.GetTokensByProfiles([]bson.ObjectId{adminProfile.Id})
+
+				msg := firebase.ManchMessage{
+					Title:    title,
+					Message:  msgStr,
+					DeepLink: deepLink,
+					Id:       notification.NId,
+				}
+
+				fmt.Printf("\nGCM Message %+v\n", msg)
+				if tokens != nil {
+					for _, token := range tokens {
+						go firebase.SendMessage(msg, token.Token, notification)
+					}
+				} else {
+					fmt.Printf("No token\n")
+				}
+
+			}
 		}
 	}
 
