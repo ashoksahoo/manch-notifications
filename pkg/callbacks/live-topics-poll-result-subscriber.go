b3504d33084fbbc6b313f0a124d788838dc75815
Ravi Shankar <ravi.shankar@manch.app>
2019-03-26 20:11:25 +0530

    change granularity name

--- pkg/callbacks/live-topics-poll-result-subscriber.go
+++ pkg/callbacks/live-topics-poll-result-subscriber.go
@@ -12,7 +12,7 @@ import (
 func LiveTopicsPollResultCB(subj, reply string, data *subscribers.LiveTopicPoll) {
 	fmt.Printf("Received a live topics poll result on subject %s! with Data %+v\n", subj, data)
 
-	granularity := "ipl"
+	granularity := "IPL2019"
 	loc, _ := time.LoadLocation("Asia/Kolkata")
 	granularityStart := time.Date(2019, 3, 23, 20, 0, 0, 0, loc)
 	granularityEnd := time.Date(2019, 5, 19, 11, 59, 59, 999, loc)
