5b0a1ce541be277f480d68ed5dab8ab6bac8c04f
Ravi Shankar <sravi4701@gmail.com>
2019-06-06 16:22:35 +0530

    fix vote schedule no.

--- pkg/callbacks/post-moderated-subscriber.go
+++ pkg/callbacks/post-moderated-subscriber.go
@@ -61,32 +61,24 @@ func PostModeratedSubscriberCB(subj, reply string, p *subscribers.Post) {
 	}
 	j := 0
 	fmt.Println("no_of_votes: ", no_of_votes)
-	t := utils.SplitTimeInRange(1, 60*24, no_of_votes, time.Minute)
+	t := utils.SplitTimeInRange(1, 90, no_of_votes, time.Minute)
 	for k := 0; j < no_of_votes; j, k = j+1, k+1 {
 		vote := mongo.CreateVotesSchedulePost(t[k], bson.ObjectIdHex(p.Id), bson.ObjectIdHex(botProfilesIds[j]))
+		fmt.Printf("Adding \n%+v\n", vote)
 		mongo.AddVoteSchedule(vote)
 	}
 
-	// randomVotes := utils.Random(5, 20)
-	// no_of_votes += randomVotes
-	// fmt.Println("no_of_votes: ", no_of_votes)
-	// t = utils.SplitTimeInRange(30, 2*24*60, randomVotes, time.Minute)
-	// for k := 0; j < no_of_votes; j, k = j+1, k+1 {
-	// 	vote := mongo.CreateVotesSchedulePost(t[k], bson.ObjectIdHex(p.Id), bson.ObjectIdHex(botProfilesIds[j]))
-	// 	mongo.AddVoteSchedule(vote)
-	// }
+	// schedule shares on posts
+	var no_of_shares int
+	no_of_shares = utils.Random(5, 10)
 
-	// // schedule shares on posts
-	// var no_of_shares int
-	// no_of_shares = utils.Random(5, 10)
-
-	// j = 0
-	// fmt.Println("no_of_shares: ", no_of_shares)
-	// t = utils.SplitTimeInRange(1, 240, no_of_shares, time.Minute)
-	// for k := 0; j < no_of_shares; j, k = j+1, k+1 {
-	// 	share := mongo.CreateShareSchedule(t[k], bson.ObjectIdHex(p.Id), bson.ObjectIdHex(botProfilesIds[j]))
-	// 	mongo.AddShareSchedule(share)
-	// }
+	j = 0
+	fmt.Println("no_of_shares: ", no_of_shares)
+	t = utils.SplitTimeInRange(1, 240, no_of_shares, time.Minute)
+	for k := 0; j < no_of_shares; j, k = j+1, k+1 {
+		share := mongo.CreateShareSchedule(t[k], bson.ObjectIdHex(p.Id), bson.ObjectIdHex(botProfilesIds[j]))
+		mongo.AddShareSchedule(share)
+	}
 
 	fmt.Printf("Processed a post on subject %s! with Post ID %s\n", subj, p.Id)
 }
